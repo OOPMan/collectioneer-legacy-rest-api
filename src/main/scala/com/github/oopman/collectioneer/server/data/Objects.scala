1604e456327f0cfaeb3bb175f3b2686f11a667fa
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2018-05-24 20:43:43 +0200

    Began implementing QueryBuilder to assist with generating queries in a conditional fashion using Options. See https://github.com/getquill/quill/issues/745

--- src/main/scala/com/github/oopman/collectioneer/server/data/Objects.scala
+++ src/main/scala/com/github/oopman/collectioneer/server/data/Objects.scala
@@ -16,9 +16,59 @@ class Objects[Dialect <: SqlIdiom, Naming <: NamingStrategy](val context: JdbcCo
     * @param left LocalDateTime to compare with
     */
   implicit class LocalDateTimeQuotes(left: LocalDateTime) {
-    def >(right: LocalDateTime): Quoted[Boolean] = quote(infix"$left > $right".as[Boolean])
-    def >=(right: LocalDateTime): Quoted[Boolean] = quote(infix"$left >= $right".as[Boolean])
-    def <(right: LocalDateTime): Quoted[Boolean] = quote(infix"$left < $right".as[Boolean])
-    def <=(right: LocalDateTime): Quoted[Boolean] = quote(infix"$left <= $right".as[Boolean])
+    def >(right: LocalDateTime) = quote(infix"$left > $right".as[Boolean])
+    def >=(right: LocalDateTime) = quote(infix"$left >= $right".as[Boolean])
+    def <(right: LocalDateTime) = quote(infix"$left < $right".as[Boolean])
+    def <=(right: LocalDateTime) = quote(infix"$left <= $right".as[Boolean])
+  }
+
+  type NullOption[T] = Option[Option[T]]
+  type QuotedQuery[T] = Quoted[Query[T]]
+  type QuotedQueryTransform[T] = Quoted[Query[T] => Query[T]]
+  type QuotedQueryTransformWithValue[T, U] = Quoted[(Query[T], U) => Query[T]]
+
+  case class QueryBuilder[T](quillQuery: QuotedQuery[T]) {
+
+    def ifDefined[U: Encoder](value: Option[U])
+                             (f: QuotedQueryTransformWithValue[T, U]): QueryBuilder[T] = {
+      value match {
+        case None => this
+        case Some(v) => QueryBuilder {
+          quote {
+            f(quillQuery, lift(v))
+          }
+        }
+      }
+    }
+
+    def ifEmpty[U: Encoder](value: Option[U])
+                           (f: QuotedQueryTransform[T]): QueryBuilder[T] = {
+      value match {
+        case Some(_) => this
+        case None => QueryBuilder(quote(f(quillQuery)))
+      }
+    }
+
+    def ifInnerDefined[U: Encoder](value: NullOption[U])
+                                  (f: QuotedQueryTransformWithValue[T, U]): QueryBuilder[T] = {
+      value match {
+        case Some(Some(v)) => QueryBuilder {
+          quote {
+            f(quillQuery, lift(v))
+          }
+        }
+        case _ => this
+      }
+    }
+
+    def ifInnerEmpty[U: Encoder](value: NullOption[U])
+                                (f: QuotedQueryTransform[T]): QueryBuilder[T] = {
+      value match {
+        case Some(None) => QueryBuilder(quote(f(quillQuery)))
+        case _ => this
+      }
+    }
+
+    def build: QuotedQuery[T] = quillQuery
   }
 }
